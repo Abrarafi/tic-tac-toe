638ec7fef1431b39ee6ca2d952017e25e5eae1c4
Abrarafi <rafi.ahrijm@gmail.com>
2025-06-08 18:22:11 +0600

    added eventListener

--- vanilla-javascript/js/app.js
+++ vanilla-javascript/js/app.js
@@ -1,1 +1,38 @@
-console.log("hello, world!");
+const App = {
+  $: {
+    menu: document.querySelector('[data-id = "menu"]'),
+    menuItems: document.querySelector('[data-id = "menu-items"]'),
+    resetBtn: document.querySelector('[data-id = "reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id = "new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id = "square"]'),
+  },
+
+  init() {
+    App.registerEventListener();
+  },
+
+  registerEventListener() {
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+
+    //todo
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("reset button clicked");
+    });
+
+    //todo
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("new round button clicked");
+    });
+    
+    //todo
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log(`square ${event.target.id} is clicked`);
+      });
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
