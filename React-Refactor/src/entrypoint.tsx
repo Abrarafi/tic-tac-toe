a5dafa72344ddb43b80c1ff32b56a65b41ce0ba2
abrarafi <rafi.ahrijm@gmail.com>
2025-06-28 12:10:03 +0600

    typescript setup completed

--- React-Refactor/src/entrypoint.tsx
+++ React-Refactor/src/entrypoint.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import {createRoot} from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
 const rootElement = document.getElementById("react-root");
+
+if(!rootElement) throw new Error("react app not configured correctly");
+
 const root = createRoot(rootElement);
 
 root.render(
