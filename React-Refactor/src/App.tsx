4d3d9f5f1683cb44a7eccbcfdb1f35faad01f69a
abrarafi <rafi.ahrijm@gmail.com>
2025-06-29 17:34:21 +0600

    setup Local storage and migrated utility code to utils.ts

--- React-Refactor/src/App.tsx
+++ React-Refactor/src/App.tsx
@@ -5,78 +5,12 @@ import Menu from "./components/Menu";
 import Modal from "./components/Modal";
 import { GameState, Player } from "./types";
 import classNames from "classnames";
+import { useLocalStorage } from "./useLocalStorage";
+import { deriveGame, deriveStats } from "./utils";
 
-const players = [
-  {
-    id: 1,
-    name: "Player 1",
-    iconClass: "fa-x",
-    colorClass: "turquoise",
-  },
-  {
-    id: 2,
-    name: "Player 2",
-    iconClass: "fa-o",
-    colorClass: "yellow",
-  },
-];
-
-function deriveGame(state: GameState) {
-  const currentPlayer = players[state.currentGameMoves.length % 2];
-
-  const winningPatterns = [
-    [1, 2, 3],
-    [1, 5, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 5, 7],
-    [3, 6, 9],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
-
-  let winner = null;
-  for (const player of players) {
-    const selectedSquareIds = state.currentGameMoves
-      .filter((move) => move.player.id === player.id)
-      .map((move) => move.squareId);
-
-    for (const pattern of winningPatterns) {
-      if (pattern.every((v) => selectedSquareIds.includes(v))) {
-        winner = player;
-      }
-    }
-  }
-  return {
-    moves: state.currentGameMoves,
-    currentPlayer,
-    status: {
-      isComplete: winner != null || state.currentGameMoves.length === 9,
-      winner,
-    },
-  };
-}
-
-function deriveStats(state: GameState) {
-  return {
-    playerWithStats: players.map((player) => {
-      const wins = state.history.currentRoundGames.filter(
-        (game) => game.status.winner?.id === player.id
-      ).length;
-
-      return {
-        ...player,
-        wins,
-      };
-    }),
-    ties: state.history.currentRoundGames.filter(
-      (game) => game.status.winner === null
-    ).length,
-  };
-}
 
 export default function App() {
-  const [state, setState] = useState<GameState>({
+  const [state, setState] = useLocalStorage<GameState>("game-state-key",{
     currentGameMoves: [],
     history: {
       currentRoundGames: [],
@@ -128,8 +62,16 @@ export default function App() {
       <main>
         <div className="grid" data-id="grid">
           <div className="turn yellow" data-id="turn">
-            <i className="fa-solid fa-x"></i>
-            <p>Player 1, you're up!</p>
+            <i
+              className={classNames(
+                "fa-solid",
+                game.currentPlayer.colorClass,
+                game.currentPlayer.iconClass
+              )}
+            ></i>
+            <p className={classNames(game.currentPlayer.colorClass)}>
+              Player {game.currentPlayer.id}, you're up!
+            </p>
           </div>
 
           <Menu onAction={(action) => resetGame(action === "new-round")} />
